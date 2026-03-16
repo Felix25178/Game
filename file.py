f12a5c818939d95b4b68ecb455d7f3dda1f227ab
ALex G <120275439+Felix25178@users.noreply.github.com>
2026-03-15 18:45:04 -0700

    file.py

--- file.py
+++ file.py
@@ -3,10 +3,12 @@ import random
 
 def instructions():
     print("""
-    _________________
-    |Welcome to GAME|
-    -----------------
     
+    _____________________
+    :::::::::::::::::::::
+    {WELCOME TO GEOPARDY}
+    *********************
+    _____________________
     
     In this game you will choose a category and then answer a question in the category of your choosing.
     If you get it right you will be awarded points, get enough points and you win.
@@ -14,19 +16,7 @@ def instructions():
     Lets begin
     """)
 
-def scores():
-    score = 0
-    return score
 
-def add(score):
-    int(score)
-    score += 10
-    return score
-
-def subtract(score):
-    int(score)
-    score -= 10
-    return score
 
 def options():
 
@@ -38,75 +28,509 @@ def options():
         History
         Math
         Animals
-        Pop Culture
+        General
         Video Games
     """)
-    pick = input("Answer: ").upper()
-    if pick[0] == "H":
+    pick = input("Answer: ").upper().strip()
+    if pick == "HISTORY":
         return 1
+    elif pick == "MATH":
+        return 2
+    elif pick == "ANIMALS":
+        return 3
+    elif pick == "GENERAL":
+        return 4
+    elif pick[0:5]== "VIDEO":
+        return 5
     else:
        print("That is not a category")
-       print("Please choose a category: ")
-
-
+       print("Please choose a category")
 
 
 
 def history_questions():
 
-    with open("history","r") as infile:
-        lines = infile.read().split()
-        lines = [int(x) for x in lines]
-    gen = random.randint(lines[0], lines[3])
+    loop = True
+    while loop:
+        with open("history","r") as infile:
+            lines = infile.read().split()
 
-    if gen == 0:
-        print()
-        print("What year did World War 2 end?")
-        ans = input("Answer: ")
-        if ans == "1945":
-            print("correct")
+        gen = random.choice(lines)
 
-        else:
-            print("Incorrect")
-    elif gen == 1:
-        print()
-        print("quest2")
-        ans = input("Answer: ")
+        if gen == "one":
+            loop = False
+            with open("history", "r") as r:
+                strg = r.read()
+            strg = strg.replace("one", "E")
+            with open("history", "w") as w:
+                w.write(strg)
+            print()
+            print("What year did World War 1 end?")
+            ans = input("Answer: ")
+            if ans == "1918":
 
-        if ans == "2":
-            print("correct")
+                print("CORRECT")
+                return "T"
+            else:
 
-        else:
-            print("Incorrect")
+                print("INCORRECT")
+                return "F"
+        elif gen == "two":
+            loop = False
+            with open("history", "r") as r:
+                strg = r.read()
+            strg = strg.replace("two", "E")
+            with open("history", "w") as w:
+                w.write(strg)
+            print()
+            print("What year was the Declaration of Independence signed?")
+            ans = input("Answer: ")
+            if ans == "1776":
 
-    elif gen == 2:
-        print()
-        print("quest3")
-        ans = input("Answer: ")
+                print("CORRECT")
+                return "T"
+            else:
 
-        if ans == "3":
-            print("correct")
+                print("INCORRECT")
+                return "F"
+        elif gen == "three":
+            loop = False
+            with open("history", "r") as r:
+                strg = r.read()
+            strg = strg.replace("three", "E")
+            with open("history", "w") as w:
+                w.write(strg)
+            print()
+            print("In a 1752 experiment, a man proved lighting was electrical. Whom am I referring to?")
+            ans = input("Answer: ").upper().strip()
+            if ans[0:3] == "BEN":
 
-        else:
-            print("Incorrect")
-    elif gen == 3:
-        print()
-        print("quest4")
-        ans = input("Answer: ")
-        if ans == "4":
-            print("correct")
+                print("CORRECT")
+                return "T"
+            else:
 
-        else:
-            print("Incorrect")
+                print("INCORRECT")
+                return "F"
+        elif gen == "four":
+            loop = False
+            with open("history","r") as r:
+                strg = r.read()
+            strg = strg.replace("four","E")
+            with open("history","w") as w:
+                w.write(strg)
+            print()
+            print("The Silk Road were trade routes all throughout eurasia, documented for spreading goods, diseases and _ ? ")
+            ans = input("Answer: ").upper().strip()
+            if ans== "RELIGION":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+
+        if all(x == "E" for x in lines[:4]):
+            loop = False
+            return "S"
+
+def math_questions():
+
+    loop = True
+    while loop:
+        with open("math","r") as infile:
+            lines = infile.read().split()
+
+        gen = random.choice(lines)
+
+        if gen == "one":
+            loop = False
+            with open("math", "r") as r:
+                strg = r.read()
+            strg = strg.replace("one", "E")
+            with open("math", "w") as w:
+                w.write(strg)
+            print()
+            print("What is -7+4-5*6/2")
+            ans = input("Answer: ")
+            if ans == "-18":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "two":
+            loop = False
+            with open("math", "r") as r:
+                strg = r.read()
+            strg = strg.replace("two", "E")
+            with open("math", "w") as w:
+                w.write(strg)
+            print()
+            print("Finish the pattern 16 9 4 _")
+            ans = input("Answer: ")
+            if ans == "1":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "three":
+            loop = False
+            with open("math", "r") as r:
+                strg = r.read()
+            strg = strg.replace("three", "E")
+            with open("math", "w") as w:
+                w.write(strg)
+            print()
+            print("What is the cubed root of 343?")
+            ans = input("Answer: ")
+            if ans == "7":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "four":
+            loop = False
+            with open("math","r") as r:
+                strg = r.read()
+            strg = strg.replace("four","E")
+            with open("math","w") as w:
+                w.write(strg)
+            print()
+            print("What is 3√4-2? ")
+            ans = input("Answer: ")
+            if ans == "4":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+
+        if all(x == "E" for x in lines[:4]):
+            loop = False
+            return "S"
 
 
+def anim_questions():
+
+    loop = True
+    while loop:
+        with open("animals","r") as infile:
+            lines = infile.read().split()
+
+        gen = random.choice(lines)
+
+        if gen == "one":
+            loop = False
+            with open("animals", "r") as r:
+                strg = r.read()
+            strg = strg.replace("one", "E")
+            with open("animals", "w") as w:
+                w.write(strg)
+            print()
+            print("The fastest known aquatic animal with a burst top speed of 68 mph is the _")
+            ans = input("Answer: ").upper().strip()
+            if ans == "SAILFISH":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "two":
+            loop = False
+            with open("animals", "r") as r:
+                strg = r.read()
+            strg = strg.replace("two", "E")
+            with open("animals", "w") as w:
+                w.write(strg)
+            print()
+            print("Bats, dolphins, and shrews are all capable of using _")
+            ans = input("Answer: ").upper().strip()
+            if ans[0:4] == "ECHO":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "three":
+            loop = False
+            with open("animals", "r") as r:
+                strg = r.read()
+            strg = strg.replace("three", "E")
+            with open("animals", "w") as w:
+                w.write(strg)
+            print()
+            print("The oldest living animal is the _ which are estimated to live up to 10,000 to 15,000 years.")
+            ans = input("Answer: ").upper().strip()
+            if ans[0:3] == "GLA":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "four":
+            loop = False
+            with open("animals","r") as r:
+                strg = r.read()
+            strg = strg.replace("four","E")
+            with open("animals","w") as w:
+                w.write(strg)
+            print()
+            print("""
+            What animal is this?
+             
+               ___---- _  -*-_
+             /             ˆ  _}
+             | /  ____--[  ]
+             |_#          |_#
+            
+                  """)
+            ans = input("Answer: ").upper().strip()
+            if ans == "BEAR":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+
+        if all(x == "E" for x in lines[:4]):
+            loop = False
+            return "S"
+
+
+
+def video_questions():
+
+    loop = True
+    while loop:
+        with open("video","r") as infile:
+            lines = infile.read().split()
+
+        gen = random.choice(lines)
+
+        if gen == "one":
+            loop = False
+            with open("video", "r") as r:
+                strg = r.read()
+            strg = strg.replace("one", "E")
+            with open("video", "w") as w:
+                w.write(strg)
+            print()
+            print("How many mandatory bosses are in Terraria? Counting Lunar Pillars as one. ")
+            ans = input("Answer: ")
+            if ans == "10":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "two":
+            loop = False
+            with open("video", "r") as r:
+                strg = r.read()
+            strg = strg.replace("two", "E")
+            with open("video", "w") as w:
+                w.write(strg)
+            print()
+            print("The protagonist of GTAIV is _")
+            ans = input("Answer: ").upper().strip()
+            if ans[0:4] == "NIKO":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "three":
+            loop = False
+            with open("video", "r") as r:
+                strg = r.read()
+            strg = strg.replace("three", "E")
+            with open("video", "w") as w:
+                w.write(strg)
+            print()
+            print("A stack in MINECRAFT is _")
+            ans = input("Answer: ")
+            if ans == "64":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "four":
+            loop = False
+            with open("video","r") as r:
+                strg = r.read()
+            strg = strg.replace("four","E")
+            with open("video","w") as w:
+                w.write(strg)
+            print()
+            print("In Wii sports there are 5 sports, bowling, golf, boxing, baseball, and _ ")
+            ans = input("Answer: ").upper().strip()
+            if ans == "TENNIS":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+
+        if all(x == "E" for x in lines[:4]):
+            loop = False
+            return "S"
+
+
+
+def general_questions():
+
+    loop = True
+    while loop:
+        with open("general","r") as infile:
+            lines = infile.read().split()
+
+        gen = random.choice(lines)
+
+        if gen == "one":
+            loop = False
+            with open("general", "r") as r:
+                strg = r.read()
+            strg = strg.replace("one", "E")
+            with open("general", "w") as w:
+                w.write(strg)
+            print()
+            print("How many milliliters are in a liter? ")
+            ans = input("Answer: ")
+            if ans == "1000":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "two":
+            loop = False
+            with open("general", "r") as r:
+                strg = r.read()
+            strg = strg.replace("two", "E")
+            with open("general", "w") as w:
+                w.write(strg)
+            print()
+            print("An economic _ refers to when prices far exceed their real value, causing markets to crash")
+            ans = input("Answer: ").upper().strip()
+            if ans == "BUBBLE":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "three":
+            loop = False
+            with open("general", "r") as r:
+                strg = r.read()
+            strg = strg.replace("three", "E")
+            with open("general", "w") as w:
+                w.write(strg)
+            print()
+            print("Earths history is divided in to 4 major eras, precambrian, paleozoic, _, and cenozoic")
+            ans = input("Answer: ").upper().strip()
+            if ans == "MESOZOIC":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+        elif gen == "four":
+            loop = False
+            with open("general","r") as r:
+                strg = r.read()
+            strg = strg.replace("four","E")
+            with open("general","w") as w:
+                w.write(strg)
+            print()
+            print("Dogs, bears, raccoons, and badgers belong to the suborder of Carnivora called _")
+            ans = input("Answer: ").upper().strip()
+            if ans == "CANIFORMIA":
+
+                print("CORRECT")
+                return "T"
+            else:
+
+                print("INCORRECT")
+                return "F"
+
+        if all(x == "E" for x in lines[:4]):
+            loop = False
+            return "S"
 
 def play_again():
 
-    again = input("Would you like to play again? ")
+    again = input("Would you like to play again? : ")
     again.lower()
 
     if again[0] == "y" or again[0] == "s":
          return True
     else:
         return False
+
+def reset():
+    with open("history", "w") as w:
+        w.write("""one
+two
+three
+four""")
+    with open("math", "w") as w:
+        w.write("""one
+two
+three
+four""")
+    with open("animals", "w") as w:
+        w.write("""one
+two
+three
+four""")
+    with open("video", "w") as w:
+        w.write("""one
+two
+three
+four""")
+    with open("general", "w") as w:
+        w.write("""one
+two
+three
+four""")
+
+def roll():
+    die1= random.randint(1,6)
+    die2 = random.randint(1,6)
+    print(f"You rolled a {die1} and a {die2}.")
+    sum = int(die1) + int(die2)
+    return sum
